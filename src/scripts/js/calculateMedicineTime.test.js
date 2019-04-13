75ce90c5d55a2440179bc419ff6a8388c18ebbd3
Igor Trindade <igorhideki@gmail.com>
2019-04-13 12:36:13 -0400

    add função que calcula intervalo do remédio

---
+++ src/scripts/js/calculateMedicineTime.test.js
@@ -0,0 +1,22 @@
+import calculateMedicineTime from './calculateMedicineTime'
+
+describe('calculateMedicineTime', () => {
+  const dateNow = new Date()
+  const timesInterval = 3
+
+  test('should call the function calculateMedicineTime and receive the result of type "array"', () => {
+    expect(Array.isArray(calculateMedicineTime(dateNow, timesInterval))).toBe(true)
+  })
+
+  test('should call the function calculateMedicineTime and the length of the result must be equal to the time interval', () => {
+    const dateNow = new Date()
+    const timesInterval = 3
+    expect(calculateMedicineTime(dateNow, timesInterval).length).toEqual(timesInterval)
+  })
+
+  test('should call the function calculateMedicineTime and return the correct result', () => {
+    const dateNow = new Date(1997, 1, 1, 0, 0)
+    const timesInterval = 3
+    expect(calculateMedicineTime(dateNow, timesInterval)).toEqual(['00:00', '08:00', '16:00'])
+  })
+})
